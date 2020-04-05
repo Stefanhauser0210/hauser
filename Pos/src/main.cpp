32ba36e5dc3dcb2bce93cee3897e5598bb621a5c
Stefanhauser0210 <hauser.stefan@student.htlwrn.ac.at>
2020-04-05 21:11:56 +0200

    CLI fuer Pos

--- Pos/src/main.cpp
+++ Pos/src/main.cpp
@@ -3,7 +3,23 @@
 
 #include "CLI11.hpp"
 
-int main() {
+using namespace std;
 
+int main(int argc, char* argv[]) {
+    CLI::App app("automata");
+
+    vector<int> input{};
+    app.add_option("INPUT", input, "The input")->required();
     
+
+    string toml{"automaton.toml"};
+    app.add_option("-f, --file", toml, "Automata file", true)->required();;
+
+    bool debug{};
+    app.add_flag("-d", debug, "debug"); 
+
+    bool verbose{};
+    app.add_flag("-v", verbose, "verbose");
+
+    CLI11_PARSE(app, argc, argv);
 }
