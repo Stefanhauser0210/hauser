443ef7744d4b77d30c224953bb6b0560489f2adb
Stefanhauser0210 <hauser.stefan@student.htlwrn.ac.at>
2020-04-10 16:24:47 +0200

    toml: Zugriff auf Table definitions und dessen Inhalte

--- Pos/src/main.cpp
+++ Pos/src/main.cpp
@@ -32,7 +32,26 @@ int main(int argc, char* argv[]) {
         cout << "v" << input << " " << file << endl;
     }
 
+
+    //toml-parsen Tests
     toml::table tab = toml::parse_file(file);
 
     cout << tab["title"] << endl;
+
+    cout << tab["definitions"] << endl;
+
+    auto definitions = tab["definitions"];
+    cout << definitions << endl;
+
+    auto z0 = definitions["z0"];
+    cout << z0 << endl;
+
+    auto e = definitions["E"];
+    cout << e << endl;
+
+    auto e_array = e.as_array();
+
+    for (const auto& k : *e_array){
+        cout << k.as_string()->get() << endl;
+    }
 }
