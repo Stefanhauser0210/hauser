eff691061aa33eef80b92648ceb2b7fd545d831f
Stefanhauser0210 <hauser.stefan@student.htlwrn.ac.at>
2020-04-10 15:33:53 +0200

    Änderungen am AUtomaten, Funktion Next, Funktion load

--- Pos/include/PD_Automaton.h
+++ Pos/include/PD_Automaton.h
@@ -5,7 +5,7 @@
 
 #include "PD_Automaton.cpp"
 
-class PD_Atomaton {
+class PD_Automaton {
 
     typedef struct Transition {
         std::string next_state;
@@ -15,15 +15,18 @@ class PD_Atomaton {
     size_t transitionTableIndex(char current_state, char stack_token, char input_token);
 
     std::string title;
-    std::vector<std::string> input_alphabet;
+    std::vector<char> input_alphabet;
     std::vector<std::string> states;
     std::vector<std::string> accepted_states;
-    std::vector<std::string> stack_alphabet;
+    std::vector<char> stack_alphabet;
 
 
 
     std::unique_ptr<Transition>* transition_table;
     std::string current_state;
-    std::stack<std::string> stack;
+    std::stack<char> stack;
+
+    static PD_Automaton load(std::string file);
+    bool next(char token);
 
 };
