7dc65f14f0f9107a536912479db7cbdb041027f9
Stefanhauser0210 <hauser.stefan@student.htlwrn.ac.at>
2020-04-12 19:49:39 +0200

    Struktur vom Automaten durchgeplant, .h-Datei angepasst

--- Pos/include/PD_Automaton.h
+++ Pos/include/PD_Automaton.h
@@ -13,6 +13,9 @@ class PD_Automaton {
     } Transition;
 
     size_t transitionTableIndex(char current_state, char stack_token, char input_token);
+    const std::shared_ptr<Transition>& getTransition(std::string current_state, char stack_token, char input_token) const;
+    void transitionTo(const std::shared_ptr<Transition>);
+    bool next(char);
 
     std::string title;
     std::vector<char> input_alphabet;
@@ -21,12 +24,17 @@ class PD_Automaton {
     std::vector<char> stack_alphabet;
 
 
+    static PD_Automaton fromFile(const std::string& file);
+
+    PD_Automaton(const PD_Automaton&);
+    virtual ~PD_Automaton();
 
     std::unique_ptr<Transition>* transition_table;
     std::string current_state;
     std::stack<char> stack;
 
     static PD_Automaton load(std::string file);
-    bool next(char token);
 
+    bool check(const std::string&);
+    std::string getTitle() const noexcept;
 };
