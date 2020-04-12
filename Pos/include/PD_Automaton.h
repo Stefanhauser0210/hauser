f5f9545955d9758434890fe0c6bc082107523c97
Stefanhauser0210 <hauser.stefan@student.htlwrn.ac.at>
2020-04-12 20:59:58 +0200

    Fehler in der Klasse PD_Automaton behoben, Funktion load angefangen

--- Pos/include/PD_Automaton.h
+++ Pos/include/PD_Automaton.h
@@ -6,35 +6,37 @@
 #include <stack>
 
 
+
 class PD_Automaton {
-
+private:
     typedef struct Transition {
-        std::string next_state;
-        std::string write_back;
+        Transition(std::string next_state, std::string write_back) : next_state_{next_state}, write_back_{write_back} {}
+        std::string next_state_;
+        std::string write_back_;
     } Transition;
+    
+    PD_Automaton() = default;  
 
-    size_t transitionTableIndex(char current_state, char stack_token, char input_token);
+    size_t transitionTableIndex(std::string current_state, char stack_token, char input_token) const;
     const std::shared_ptr<Transition>& getTransition(std::string current_state, char stack_token, char input_token) const;
     void transitionTo(const std::shared_ptr<Transition>);
     bool next(char);
 
-    std::string title;
-    std::vector<char> input_alphabet;
-    std::vector<std::string> states;
-    std::vector<std::string> accepted_states;
-    std::vector<char> stack_alphabet;
+    std::string title_;
+    std::vector<char> input_alphabet_;
+    std::vector<std::string> states_;
+    std::vector<std::string> accepted_states_;
+    std::vector<char> stack_alphabet_;
 
+    std::shared_ptr<Transition>* transition_table_;
+    std::string current_state_;
+    std::stack<char> stack_;
 
-    static PD_Automaton fromFile(const std::string& file);
+public:
+    static PD_Automaton load(const std::string& file); 
 
-    PD_Automaton(const PD_Automaton&);
-    virtual ~PD_Automaton();
-
-    std::unique_ptr<Transition>* transition_table;
-    std::string current_state;
-    std::stack<char> stack;
-
-    static PD_Automaton load(std::string file);
+    PD_Automaton(const PD_Automaton&); 
+    virtual ~PD_Automaton();  
 
     bool check(const std::string&);
     std::string getTitle() const noexcept;
--- Pos/src/PD_Automaton.cpp
+++ Pos/src/PD_Automaton.cpp
@@ -3,13 +3,22 @@
 #include "toml++/toml.h"
 #include <algorithm>
 #include <memory>
+#include <iostream>
 
 using namespace std::string_view_literals;
 
-
-PD_Automaton::PD_Automaton(const PD_Automaton& other) {}
+PD_Automaton::PD_Automaton([[maybe_unused]] const PD_Automaton& other) {}
 
 
 PD_Automaton::~PD_Automaton() {
-    delete[] transition_table;
+    delete[] transition_table_;
+}
+
+
+PD_Automaton PD_Automaton::load(const std::string& file) {
+    toml::table config = toml::parse_file(file);
+    std::cout << config;
+
+    PD_Automaton automaton;
+    return automaton;
 }
