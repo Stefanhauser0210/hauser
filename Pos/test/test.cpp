5214e93d819f781c31294bb3b222ce738a1efcd6
Stefanhauser0210 <hauser.stefan@student.htlwrn.ac.at>
2020-04-17 18:20:44 +0200

    test_exe: jeder Testcase hat jetzt eigenen Automaten damit Stack richtig bleibt

--- Pos/test/test.cpp
+++ Pos/test/test.cpp
@@ -14,15 +14,22 @@ TEST_CASE("Automaton Page 45 - More 0 then 1"){
     Logger::debug_logger->set_level(spdlog::level::off);
 
     string file{"./../automaton.toml"};
-    PD_Automaton automaton{PD_Automaton::load(file)};
+    PD_Automaton automaton1{PD_Automaton::load(file)};
+    PD_Automaton automaton2{PD_Automaton::load(file)};
+    PD_Automaton automaton3{PD_Automaton::load(file)};
+    PD_Automaton automaton4{PD_Automaton::load(file)};
+    PD_Automaton automaton5{PD_Automaton::load(file)};
+    PD_Automaton automaton6{PD_Automaton::load(file)};
+    PD_Automaton automaton7{PD_Automaton::load(file)};
+    PD_Automaton automaton8{PD_Automaton::load(file)};
+    
+    CHECK(automaton1.check("11000"));
+    CHECK(automaton2.check("00011"));
+    CHECK(automaton3.check("00110"));
+    CHECK(automaton4.check("10001"));
 
-    CHECK(automaton.check("11000"));
-    CHECK(automaton.check("00011"));
-    CHECK(automaton.check("00110"));
-    CHECK(automaton.check("10001"));
-
-    CHECK(!automaton.check("00111"));
-    CHECK(!automaton.check("11100"));
-    CHECK(!automaton.check("11001"));
-    CHECK(!automaton.check("01110"));
+    CHECK(!automaton5.check("00111"));
+    CHECK(!automaton6.check("11100"));
+    CHECK(!automaton7.check("11001"));
+    CHECK(!automaton8.check("01110"));
 }
