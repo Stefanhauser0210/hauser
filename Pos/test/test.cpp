ce2163bbb9f50439532ba7e9a53b094d8302af93
Stefanhauser0210 <hauser.stefan@student.htlwrn.ac.at>
2020-04-21 17:50:13 +0200

    Doctest: mehr testcases für automaton.toml mit verschiedenen Längen

--- Pos/test/test.cpp
+++ Pos/test/test.cpp
@@ -8,44 +8,150 @@
 
 using namespace std;
 
+TEST_CASE("Automaton Page 45 - More 0 then 1: length: 3"){
+    Logger::logger->set_level(spdlog::level::off);
+    Logger::debug_logger->set_level(spdlog::level::off);
 
-TEST_CASE("Automaton Page 45 - More 0 then 1"){
+    string file{"./../automaton.toml"};
+    PD_Automaton automaton{PD_Automaton::load(file)};
+
+    SUBCASE("length: 3 chars, begin:1, end:0 - accepted"){
+        CHECK(automaton.check("100"));
+    }
+
+    SUBCASE("length: 3 chars, begin:0, end:1 - accepted"){
+        CHECK(automaton.check("001"));
+    }   
+
+    SUBCASE("length: 3 chars, begin:0, end:0 - accepted"){
+        CHECK(automaton.check("010"));
+    }
+
+    SUBCASE("length: 3 chars, begin:1, end:0 - rejected"){
+        CHECK(!automaton.check("110"));
+    }
+
+    SUBCASE("length: 3 chars, begin:0, end:1 - rejected"){
+        CHECK(!automaton.check("011"));
+    }
+
+    SUBCASE("length: 3 chars, begin:1, end:1 - rejected"){
+        CHECK(!automaton.check("101"));
+    }
+
+    SUBCASE("length: 3 chars, only 0"){
+        CHECK(automaton.check("000"));
+    }
+
+    SUBCASE("length: 3 chars, only 1"){
+        CHECK(!automaton.check("111"));
+    }  
+
+}
+
+TEST_CASE("Automaton Page 45 - More 0 then 1: length: 4"){
+    Logger::logger->set_level(spdlog::level::off);
+    Logger::debug_logger->set_level(spdlog::level::off);
+
+    string file{"./../automaton.toml"};
+    PD_Automaton automaton{PD_Automaton::load(file)};
+
+    SUBCASE("length: 4 chars, begin:1, end:0 - accepted"){
+        CHECK(automaton.check("1000"));
+    }
+
+    SUBCASE("length: 4 chars, begin:0, end:1 - accepted"){
+        CHECK(automaton.check("0001"));
+    }   
+
+    SUBCASE("length: 4 chars, begin:0, end:0 - accepted"){
+        CHECK(automaton.check("0100"));
+    }
+
+    SUBCASE("length: 4 chars, begin:1, end:0 - rejected"){
+        CHECK(!automaton.check("1110"));
+    }
+
+    SUBCASE("length: 4 chars, begin:0, end:1 - rejected"){
+        CHECK(!automaton.check("0111"));
+    }
+
+    SUBCASE("length: 4 chars, begin:1, end:1 - rejected"){
+        CHECK(!automaton.check("1011"));
+    } 
+
+}
+
+TEST_CASE("Automaton Page 45 - More 0 then 1: length: 5"){
     Logger::logger->set_level(spdlog::level::off);
     Logger::debug_logger->set_level(spdlog::level::off);
 
     string file{"./../automaton.toml"};
     PD_Automaton automaton{PD_Automaton::load(file)};
     
-    SUBCASE("Test dependency of begin and end of the word: begin:1, end:0 - accepted"){
+    SUBCASE("length: 5 chars, begin:1, end:0 - accepted"){
         CHECK(automaton.check("11000"));
     }
 
-    SUBCASE("Test dependency of begin and end of the word: begin:0, end:1 - accepted"){
+    SUBCASE("length: 5 chars, begin:0, end:1 - accepted"){
         CHECK(automaton.check("00011"));
     }
 
-    SUBCASE("Test dependency of begin and end of the word: begin:0, end:0 - accepted"){
+    SUBCASE("length: 5 chars, begin:0, end:0 - accepted"){
         CHECK(automaton.check("00110"));
     }
 
-    SUBCASE("Test dependency of begin and end of the word: begin:1, end:1 - accepted"){
+    SUBCASE("length: 5 chars, begin:1, end:1 - accepted"){
         CHECK(automaton.check("10001"));
     }
    
-    SUBCASE("Test dependency of begin and end of the word: begin:1, end:0 - rejected"){
+    SUBCASE("length: 5 chars, begin:1, end:0 - rejected"){
         CHECK(!automaton.check("11100"));
     }
 
-    SUBCASE("Test dependency of begin and end of the word: begin:0, end:1 - rejected"){
+    SUBCASE("length: 5 chars, begin:0, end:1 - rejected"){
         CHECK(!automaton.check("00111"));
     }
 
-    SUBCASE("Test dependency of begin and end of the word: begin:0, end:0 - rejected"){
+    SUBCASE("length: 5 chars, begin:0, end:0 - rejected"){
         CHECK(!automaton.check("01110"));
     }
 
-    SUBCASE("Test dependency of begin and end of the word: begin:1, end:1 - rejected"){
+    SUBCASE("length: 5 chars, begin:1, end:1 - rejected"){
         CHECK(!automaton.check("11001"));
     }      
+
+}
+
+TEST_CASE("Automaton Page 45 - More 0 then 1: same number of 0 and 1"){
+    Logger::logger->set_level(spdlog::level::off);
+    Logger::debug_logger->set_level(spdlog::level::off);
+
+    string file{"./../automaton.toml"};
+    PD_Automaton automaton{PD_Automaton::load(file)};
+    
+    SUBCASE("length: 2 chars, begin:1, end:0"){
+        CHECK(!automaton.check("10"));
+    }
+
+    SUBCASE("length: 2 chars, begin:0, end:1"){
+        CHECK(!automaton.check("01"));
+    }
+
+    SUBCASE("length: 4 chars, begin:0, end:1"){
+        CHECK(!automaton.check("0101"));
+    }
+
+    SUBCASE("length: 4 chars, begin:1, end:0"){
+        CHECK(!automaton.check("1100"));
+    }
+
+    SUBCASE("length: 4 chars, begin:0, end:0"){
+        CHECK(!automaton.check("0110"));
+    }
+
+    SUBCASE("length: 4 chars, begin:1, end:1"){
+        CHECK(!automaton.check("1001"));
+    }
 
 }
